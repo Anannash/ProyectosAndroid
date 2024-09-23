ac74c9b4c45ec92a841bf1f65a4566a1993238db
AndreaMCH24 <aome2005.kirara7@gmail.com>
2024-09-23 14:37:03 -0600

    kk

--- ListasVideos/app/src/main/java/tecnm/chihuahua2/desarrollomovil/listasvideos/Videos.java
+++ ListasVideos/app/src/main/java/tecnm/chihuahua2/desarrollomovil/listasvideos/Videos.java
@@ -1,8 +1,6 @@
 package tecnm.chihuahua2.desarrollomovil.listasvideos;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnListas, btnVideos;
+public class Videos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_videos);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnListas = findViewById(R.id.btnListas);
-        btnVideos = findViewById(R.id.btnVideos);
-
-        btnListas.setOnClickListener(v -> {
-            startActivity(new Intent(this, Listas.class));
-        });
-
-        btnVideos.setOnClickListener(v -> {
-            startActivity(new Intent(this, Videos.class));
-        });
-
-
     }
 }
